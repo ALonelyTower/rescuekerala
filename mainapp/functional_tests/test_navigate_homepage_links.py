e6ad96a69075cedb99d99658bed8007e0dce16f0
ALonelyTower <jason.lee.wong@outlook.com>
2018-08-20 21:28:53 -0700

    Refactor test to be more accurate, and testing different Travis config.

--- mainapp/functional_tests/test_navigate_homepage_links.py
+++ mainapp/functional_tests/test_navigate_homepage_links.py
@@ -19,14 +19,13 @@ class HomePageHyperLinksTest(StaticLiveServerTestCase):
         super().tearDownClass()
 
     def test_navigate_from_home_to_requestforhelp(self):
-        homepage_requesthelp_link_text = "സഹായം അഭ്യര്‍ഥിക്കാന്‍" 
+        requesthelp_link_malayalam_text = "സഹായം അഭ്യര്‍ഥിക്കാന്‍" 
         requestpage_header_english_text = "Request For Help"
-        requestpage_header_malayalam_text = "സഹായം അഭ്യര്‍ഥിക്കാന്‍"
 
         self.selenium.get(self.live_server_url)
-        request_help_link = self.selenium.find_element_by_partial_link_text(homepage_requesthelp_link_text)
+        request_help_link = self.selenium.find_element_by_partial_link_text(requesthelp_link_malayalam_text)
         request_help_link.click()
 
         self.assertIn("/request/", self.selenium.current_url)
         self.assertIn(requestpage_header_english_text, self.selenium.page_source)
-        self.assertIn(requestpage_header_malayalam_text, self.selenium.page_source)
+        self.assertIn(requesthelp_link_malayalam_text, self.selenium.page_source)
